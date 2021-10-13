f8e42a063bec669b9a1f206dc0fda1698ffa96d1
Alexander Sidorov <pl4zie@gmail.com>
2021-10-13 14:19:38 +0300

    Removed /games directory

--- brain_games/scripts/gameplay.py
+++ brain_games/scripts/gameplay.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
-
 def main():
     pass
+
 
 # function receives a string and a function
 def play(question, game):
@@ -9,8 +9,8 @@ def play(question, game):
     print('Welcome to the Brain Games!')
     name = ''
     while name == '':
-            print('May I have your name? ', end='')
-            name = input()
+        print('May I have your name? ', end='')
+        name = input()
     print('Hello, {}!'.format(name))
     # every game has its own question to answer
     print(question)
@@ -18,19 +18,17 @@ def play(question, game):
     correct_answers = 0
     while correct_answers < 3:
         # every game returns answer and value
-        final_pack = game
-        answer = final_pack[0]
-        value = final_pack[1]
+        answer, value = game
         if check_answer(answer, value) is True:
             # Counting correct answers up to 3
             correct_answers += 1
             print('Correct!')
         else:
             # Wrong answer ends the game
-            print('"{}" is wrong answer ;(. Correct answer was "{}"'.format(answer, value))
+            print('"{}" is wrong answer ;(. '.format(answer),
+                  'Correct answer was "{}"'.format(value))
             print('Let\'s try again, {}!'.format(name))
             break
-    
     if correct_answers == 3:
         print('Congratulations, {}!'.format(name))
 
