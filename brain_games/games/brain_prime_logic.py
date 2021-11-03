fa1b752eeefe5281d634df50af094072c1433681
Alexander Sidorov <pl4zie@gmail.com>
2021-11-04 00:34:22 +0300

    Refactor brain_prime

---
+++ brain_games/games/brain_prime_logic.py
@@ -0,0 +1,29 @@
+''' Prime number game.
+
+    To win, you must define whether
+    a random number is prime or not.
+'''
+from random import randint
+
+
+description = 'Answer "yes" if given number is prime. Otherwise answer "no".'
+
+
+def quest():
+    number = randint(0, 200)
+    return number
+
+
+def check(number):
+    return is_prime(number)
+
+
+def is_prime(num):
+    if num > 1:
+        for i in range(2, int(num // 2) + 1):
+            if (num % i) == 0:
+                return 'no'
+            else:
+                return 'yes'
+    else:
+        return 'no'
--- brain_games/scripts/brain_prime.py
+++ brain_games/scripts/brain_prime.py
@@ -1,38 +1,11 @@
-import random
-import prompt
+#!/usr/bin/env python
+from brain_games.scripts.engine import build_and_play
+from brain_games.games.brain_prime_logic import description, quest, check
 
 
 def main():
-    print('Welcome to the Brain Games!')
-    name = ''
-    while name == '':
-        print('May I have your name? ', end='')
-        name = input()
-    print('Hello, {}!'.format(name))
-    correct_answers = 0
-    print('Answer "yes" if given number is prime. Otherwise answer "no".')
-    while correct_answers < 3:
-        number = random.randint(0, 37)
-        quest = 'Question: ' + str(number) + ' '
-        answer = prompt.string(quest, empty=True)
-        if answer == is_prime(number):
-            correct_answers += 1
-            print('Correct!')
-        else:
-            print('"{}" is wrong answer ;(. '.format(answer),
-                  'Correct answer was "{}"'.format(is_prime(number)))
-            print('Let\'s try again, {}!'.format(name))
-            break
-    if correct_answers == 3:
-        print('Congratulations, {}!'.format(name))
+    build_and_play(description, quest, check)
 
 
-def is_prime(num):
-    if num > 1:
-        for i in range(2, int(num / 2) + 1):
-            if (num % i) == 0:
-                return 'no'
-            else:
-                return 'yes'
-    else:
-        return 'no'
+if __name__ == '__main__':
+    main()
